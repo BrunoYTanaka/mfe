405902eadaed470cce404220d09fe7fce1601d32
Bruno Y. Tanaka <bruno.tanaka@agilecontent.com>
2022-08-26 09:33:58 -0400

    AuthApp

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Marketing from "./components/Marketing";
-import Header from "./components/Header";
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+
+import Header from "./components/Header";
+import Progress from "./components/Progress";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -15,10 +19,13 @@ export default () => {
   return (
     <StylesProvider generateClassName={generateClassName}>
       <BrowserRouter>
-        <>
-          <Header />
-          <Marketing />
-        </>
+        <Header />
+        <Suspense fallback={<Progress />}>
+          <Switch>
+            <Route path="/auth" component={AuthLazy} />
+            <Route path="/" component={MarketingLazy} />
+          </Switch>
+        </Suspense>
       </BrowserRouter>
     </StylesProvider>
   );
